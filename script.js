d84b33d6e52c943e08deb57352c2bafaeae793e5
danceadventuresquad <danceadventuresquad@gmail.com>
2025-05-22 17:13:12 +1000

    Updated script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let map;
 let currentMarkers = [];
 // URL of your Apps Script web app that serves JSON
-const SCRIPT_URL = "YOUR_APPS_SCRIPT_JSON_WEB_APP_URL_HERE"; 
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyEPikMZ0IYuMbhMG2SX7vPJSUtCKHQ2URTE3fK_TnTpHc6Qo4CcdqZW_1PriKlk8U3/exec"; 
 const REFRESH_INTERVAL = 15 * 60 * 1000; // 15 minutes in milliseconds
 
 function initMap() {
